4eb32133fcaedae9aeeddfda7e4f730fbd23f087
Bancantcode <santiagobryan831@gmail.com>
2024-08-29 13:46:33 +0800

    product sample

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    extend: {
+      colors: {
+        'bg-primary': '#222220',
+        'bg-secondary': '#F0F2F1',
+        'primary-color': '#FFFFFF',
+        'secondary-color': '#8F8B8B'
+      }
+    },
+  },
+  plugins: [],
+}
+
